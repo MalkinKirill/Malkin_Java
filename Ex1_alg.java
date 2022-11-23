10f2e23f63675c6739f2056948cfd95c8b3531ec
MalkinKirill <87969437+MalkinKirill@users.noreply.github.com>
2022-11-23 12:44:17 +0300

    Add files via upload

---
+++ Ex1_alg.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ex1_alg {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int a = num.nextInt();
+        if (a > 7) {
+            System.out.println("Привет");
+        }
+    }
+}
+
+/*
+Составить алгоритм: если введенное число больше 7, то вывести “Привет”
+ */
